e4e28e28f848336fb363b2dc73ff8aa9d4806e66
calebomondi <caleb.omondi@students.jkuat.ac.ke>
2025-01-19 15:35:35 +0300

    add Contact component with social links and integrate it into Home component

--- src/components/contacts/contact.tsx
+++ src/components/contacts/contact.tsx
@@ -1,9 +1,36 @@
-//
+import { Github, Linkedin, Twitter, Mail } from "lucide-react"
 
 export default function Contact() {
   return (
-    <div>
-      
-    </div>
+    <div id="contact" className="h-auto flex items-center justify-center text-white">
+        <div className="max-w-4xl mx-auto px-4 py-2 text-center">
+          <h2 className="text-3xl font-bold mb-2">Coming Soon</h2>
+          <p className="text-lg text-white/90 mb-2">
+            Have any inquiry, comment or suggestion, let's talk
+          </p>
+          <div className="flex flex-col md:flex-row justify-center space-y-4 md:space-y-0 md:space-x-6">
+            <a href="mailto:kalebmokua@gmail.com"
+              className="inline-flex items-center justify-center bg-white text-blue-600 px-8 py-3
+                rounded-full font-medium hover:bg-blue-50 transition-all transform hover:scale-105">
+              <Mail className="mr-2" size={20} />
+              Send me an email
+            </a>
+            <div className="flex justify-center space-x-4">
+              <a href="https://github.com/calebomondi" target="_blank" className="bg-white/10 p-3 rounded-full hover:bg-white/20
+                transition-all transform hover:scale-110">
+                <Github size={24} />
+              </a>
+              <a href="https://www.linkedin.com/in/mokuakaleb/" target="_blank" className="bg-white/10 p-3 rounded-full hover:bg-white/20
+                transition-all transform hover:scale-110">
+                <Linkedin size={24} />
+              </a>
+              <a href="https://x.com/mokuakaleb" target="_blank" className="bg-white/10 p-3 rounded-full hover:bg-white/20
+                transition-all transform hover:scale-110">
+                <Twitter size={24} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
   )
 }
--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,9 +1,18 @@
-//
+import Contact from "../contacts/contact"
+import logo from '/1.png'
 
 export default function Home() {
   return (
-    <div>
-      hello world!
+    <div className="flex justify-center items-center flex-col h-screen
+    dark:from-green-900 dark:to-blue-900
+        bg-gradient-to-br from-green-400 via-yellow-600 to-yellow-600">
+        <img 
+            src={logo}
+            alt="" 
+            className="md:w-1/5 w-1/2"
+        />
+        <div className="md:w-1/2 md:line-clamp-3 text-center p-3 my-5 text-lg text-white">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic temporibus earum, ipsa ipsum asperiores a distinctio reprehenderit.</div>
+        <Contact />
     </div>
   )
 }
