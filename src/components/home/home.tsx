c1bfbf8ffc42c261094294435429303f325841a2
calebomondi <caleb.omondi@students.jkuat.ac.ke>
2025-01-20 20:14:01 +0300

    add description to Home component and update text display

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -2,6 +2,8 @@ import Contact from "../contacts/contact"
 import logo from '/1.png'
 
 export default function Home() {
+    const descr = 'A blockchain-based platform that acts as a locked savings account, enabling virtual asset owners to cultivate financial discipline by locking assets for a specified duration to prevent premature withdrawals while maintaining flexibility for emergency access and periodic lock adjustments.'
+
   return (
     <div className="flex justify-center items-center flex-col h-screen
     dark:from-green-900 dark:to-blue-900
@@ -11,7 +13,7 @@ export default function Home() {
             alt="" 
             className="md:w-1/5 w-1/2"
         />
-        <div className="md:w-1/2 md:line-clamp-3 text-center p-3 my-5 text-lg text-white">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic temporibus earum, ipsa ipsum asperiores a distinctio reprehenderit.</div>
+        <div className="md:w-1/2 md:line-clamp-5 text-center p-3 my-5 text-lg text-white">{descr}</div>
         <Contact />
     </div>
   )
