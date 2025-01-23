0c28ae761f9ff6089ce6cfe3ad3fd929e25b97c5
calebomondi <caleb.omondi@students.jkuat.ac.ke>
2025-01-23 23:01:06 +0300

    refactor Home component layout and enhance description for clarity

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -2,18 +2,26 @@ import Contact from "../contacts/contact"
 import logo from '/1.png'
 
 export default function Home() {
-    const descr = 'A blockchain-based platform that acts as a locked savings account, enabling virtual asset owners to cultivate financial discipline by locking assets for a specified duration to prevent premature withdrawals while maintaining flexibility for emergency access and periodic lock adjustments.'
+    const descr = 'We enable virtual asset owners to cultivate financial discipline by locking assets for a specified duration to prevent premature withdrawals while maintaining flexibility for emergency access and periodic lock adjustments.'
 
   return (
-    <div className="flex justify-center items-center flex-col h-screen
+    <div className="flex justify-center items-center flex-col md:h-screen
     dark:from-green-900 dark:to-blue-900
         bg-gradient-to-br from-green-500 to-yellow-600">
-        <img 
-            src={logo}
-            alt="" 
-            className="md:w-1/5 w-1/2"
-        />
-        <div className="md:w-1/2 md:line-clamp-5 text-center p-3 my-5 text-lg text-white">{descr}</div>
+        <div className="flex flex-col md:flex-row-reverse justify-evenly items-center">
+          <div className="md:w-1/2 grid place-items-center">
+            <img 
+              src={logo}
+              alt="" 
+              className=""
+            />
+          </div>
+          <div className="md:w-1/2 text-center p-3 my-5 text-lg text-white space-y-4">
+            <p className="text-5xl font-semibold">Avoid  Impulsive Spending or Trading</p>
+            <p className="text-4xl">Lock Your Savings, Invest In Your Future!</p>
+            <p>{descr}</p>
+          </div>
+        </div>
         <Contact />
     </div>
   )
