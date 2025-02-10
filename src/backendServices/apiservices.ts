e8003faf2e10b76ae5e4c30f479242cb54424fde
calebomondi <caleb.omondi@students.jkuat.ac.ke>
2025-02-10 17:18:41 +0300

    implement lockETH function in apiService for uploading teacher data; add error handling and set content type header

--- src/backendServices/apiservices.ts
+++ src/backendServices/apiservices.ts
@@ -1,8 +1,28 @@
-//import axios, {AxiosResponse} from 'axios'
-//import { API_URL } from './apiurl'
+import axios, {AxiosResponse} from 'axios'
+import { API_URL } from './apiurl'
 
 const apiService = {
-    
+    lockETH: async (parsedData:any): Promise<any> => {
+        try {
+            const response: AxiosResponse<any> = await axios.post(
+              `${API_URL}/api/admin/upload/teachers`,
+              {
+                data: parsedData
+              },
+              {
+                headers: {
+                  'Content-Type': 'application/json'
+                }
+              }
+            );
+      
+            return response.data;
+            
+          } catch (error) {
+            console.error('Error in uploadTeachers:', error);
+            throw error;
+          }
+    },
 }
 
 export default apiService;
