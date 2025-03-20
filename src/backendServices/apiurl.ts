be1b4643278fa6ff99b44498e03a28de544fe87a
calebomondi <caleb.omondi@students.jkuat.ac.ke>
2025-03-20 22:14:20 +0300

    feat: update API_URL to point to the production backend service

--- src/backendServices/apiurl.ts
+++ src/backendServices/apiurl.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:3000'
-//export const API_URL = 'https://fvkry-backend.vercel.app'
+//export const API_URL = 'http://localhost:3000'
+export const API_URL = 'https://fvkry-backend.vercel.app'
